165edd4562fe7b17b3c3bc8de87ffb128c00e837
AtomJump <peter@atomjump.com>
2016-08-17 11:58:08 +1200

    Post the message to the forum

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 	$api = new cls_plugin_api();
 
 
-    function find_layer($body)
+    function find_forum($body)
     {
         $words = str_split($body);
         foreach($words as $word) {
@@ -54,21 +54,39 @@
          $name = "Anon " . substr($_REQUEST['From'], -2);
     }
      
-    //Search the Body of the message for the first 'word@' mention - this is the layer to send to
+    //Search the Body of the message for the first 'word@' mention - this is the forum to send to
     //$_REQUEST['Body']   - sms body
- 
+   if(($forum = find_forum($_REQUEST['Body']) != false) {
 
-   $shouted = $title . $summary_description . " " . $guid;		//guid may not be url for some feeds, may need to have link
-    $your_name = $name;
-    $whisper_to = "";
-    $email = $feed['email'];
-    $ip = "92.27.10.17"; //must be something anything
-    $forum_name = $feed['aj'];
+      //We have a forum to post to
+  
 
+       $shouted = $title . $summary_description . " " . $guid;		//guid may not be url for some feeds, may need to have link
+       $your_name = $name;
+       $whisper_to = "";
+       $email = "noreply" . $_REQUEST['From'] . "@atomjump.com";
+       $ip = "92.27.10.17"; //must be something anything
+       $forum_name = $forum;
+
+       //Get the forum id
+       $forum_info = $api->get_forum_id($forum_name);
+						
+      //Send the message
+      $api->new_message($your_name, $shouted, $whisper_to, $email, $ip, $forum_info['forum_id'], false);
+						
     // now greet the sender
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message><?php echo $name ?>, thanks for the message!</Message>
+    <Message><?php echo $name ?>, thanks, your message has been posted to <?php str_replace("FORUM", $forum_name, $sms_accept_config['domain']); ?>!</Message>
 </Response>
+<?php  
+     } else {
+
+       //No body message
+?>
+<Response>
+    <Message><?php echo $name ?>, thanks, but your message is missing a forum name with an @ at the end. Please try again.</Message>
+</Response>
+<?php } ?>
